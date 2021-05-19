927110fb6b3a578071e63ed6d059252597e5be2e
Takuya UESHIN <ueshin@databricks.com>
2021-05-19 13:05:13 -0700

    Set up the integration test.

--- python/delta_sharing/tests/conftest.py
+++ python/delta_sharing/tests/conftest.py
@@ -14,16 +14,20 @@
 # limitations under the License.
 #
 import os
+from pathlib import Path
+import subprocess
+import threading
+from typing import Iterator, Optional
 
 import pytest
+from pytest import TempPathFactory
 
 from delta_sharing.delta_sharing import SharingClient
 from delta_sharing.protocol import ShareProfile
 from delta_sharing.rest_client import DataSharingRestClient
 
 
-# Make it `True` if the server is running.
-ENABLE_INTEGRATION = False
+ENABLE_INTEGRATION = len(os.environ.get("AWS_ACCESS_KEY_ID", "")) > 0
 SKIP_MESSAGE = "The integration tests are disabled."
 
 
@@ -45,3 +49,45 @@ def rest_client(profile) -> DataSharingRestClient:
 @pytest.fixture
 def sharing_client(profile) -> SharingClient:
     return SharingClient(profile)
+
+
+@pytest.fixture(scope="session", autouse=ENABLE_INTEGRATION)
+def test_server(tmp_path_factory: TempPathFactory) -> Iterator[None]:
+    pid_file: Optional[Path] = None
+    proc: Optional[subprocess.Popen] = None
+    try:
+        if ENABLE_INTEGRATION:
+            pid_file = tmp_path_factory.getbasetemp() / "delta-sharing-server.pid"
+            proc = subprocess.Popen(
+                [
+                    "./build/sbt",
+                    (
+                        "server/test:runMain io.delta.sharing.server.TestDeltaSharingServer "
+                        + str(pid_file)
+                    ),
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd="..",
+            )
+
+            ready = threading.Event()
+
+            def wait_for_server() -> None:
+                for line in proc.stdout:
+                    print(line.decode("utf-8").strip())
+                    if b"https://127.0.0.1:12345/" in line:
+                        ready.set()
+
+            threading.Thread(target=wait_for_server, daemon=True).start()
+
+            if not ready.wait(timeout=120):
+                raise TimeoutError("the server didn't start in 120 seconds")
+        yield
+    finally:
+        if ENABLE_INTEGRATION:
+            if pid_file is not None and pid_file.exists():
+                pid = pid_file.read_text()
+                subprocess.run(["kill", "-9", pid])
+            if proc is not None and proc.poll() is None:
+                proc.kill()
