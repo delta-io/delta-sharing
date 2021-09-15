f540dda45b1cf0b624c15801708fd7688e153ade
Shixiong Zhu <zsxwing@gmail.com>
2021-09-15 11:34:13 -0700

    fix style

--- python/delta_sharing/tests/test_delta_sharing.py
+++ python/delta_sharing/tests/test_delta_sharing.py
@@ -141,22 +141,12 @@ def test_list_all_tables(sharing_client: SharingClient):
         ),
         pytest.param(
             "share_azure.default.table_wasb",
-            pd.DataFrame(
-                {
-                    "c1": ["foo bar"],
-                    "c2": ["foo bar"],
-                }
-            ),
+            pd.DataFrame({"c1": ["foo bar"], "c2": ["foo bar"],}),
             id="Azure Blob Storage",
         ),
         pytest.param(
             "share_azure.default.table_abfs",
-            pd.DataFrame(
-                {
-                    "c1": ["foo bar"],
-                    "c2": ["foo bar"],
-                }
-            ),
+            pd.DataFrame({"c1": ["foo bar"], "c2": ["foo bar"],}),
             id="Azure Data Lake Storage Gen2",
         ),
     ],
