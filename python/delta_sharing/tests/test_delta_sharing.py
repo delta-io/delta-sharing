5e901d77603005c9aa3cd1084ed27dac5bc4b937
Lin Zhou <87341375+linzhou-db@users.noreply.github.com>
2022-05-12 11:42:34 -0700

    Extends Delta Sharing Metadata with field: configuration (#138)
    * Extends Delta Sharing Metadata with field: configuration
    
    * fix rest client suite

--- python/delta_sharing/tests/test_delta_sharing.py
+++ python/delta_sharing/tests/test_delta_sharing.py
@@ -70,6 +70,7 @@ def test_list_tables(sharing_client: SharingClient):
         Table(name="table1", share="share1", schema="default"),
         Table(name="table3", share="share1", schema="default"),
         Table(name="table7", share="share1", schema="default"),
+        Table(name="cdf_table_cdf_enabled", share="share1", schema="default"),
     ]
 
     tables = sharing_client.list_tables(Schema(name="default", share="share2"))
@@ -81,6 +82,7 @@ def _verify_all_tables_result(tables: Sequence[Table]):
         Table(name="table1", share="share1", schema="default"),
         Table(name="table3", share="share1", schema="default"),
         Table(name="table7", share="share1", schema="default"),
+        Table(name="cdf_table_cdf_enabled", share="share1", schema="default"),
         Table(name="table2", share="share2", schema="default"),
         Table(name="table4", share="share3", schema="default"),
         Table(name="table5", share="share3", schema="default"),
