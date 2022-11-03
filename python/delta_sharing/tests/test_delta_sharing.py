fd5f3266c137285f38e6de69e9d160dabcab8f34
Lin Zhou <lin.zhou@databricks.com>
2022-11-02 23:54:13 -0700

    fix test

--- python/delta_sharing/tests/test_delta_sharing.py
+++ python/delta_sharing/tests/test_delta_sharing.py
@@ -99,7 +99,8 @@ def _verify_all_tables_result(tables: Sequence[Table]):
         Table(name="cdf_table_missing_log", share="share8", schema="default"),
         Table(name="streaming_table_with_optimize", share="share8", schema="default"),
         Table(name="streaming_table_metadata_protocol", share="share8", schema="default"),
-        Table(name="streaming_table_read_incompatible", share="share8", schema="default"),
+        Table(name="streaming_notnull_to_null", share="share8", schema="default"),
+        Table(name="streaming_null_to_notnull", share="share8", schema="default"),
         Table(name="table_reader_version_increased", share="share8", schema="default")
     ]
 
