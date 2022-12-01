09d5cb6a9a287bad652159aefe1f785f53dad142
Lin Zhou <87341375+linzhou-db@users.noreply.github.com>
2022-12-01 13:44:04 -0800

    nit fix on the version used for testing (#218)

--- python/delta_sharing/tests/test_delta_sharing.py
+++ python/delta_sharing/tests/test_delta_sharing.py
@@ -746,7 +746,7 @@ def test_load_table_changes_as_spark(
         spark = SparkSession.builder \
             .appName("delta-sharing-test") \
             .master("local[*]") \
-            .config("spark.jars.packages", "io.delta:delta-sharing-spark_2.12:1.0.0-SNAPSHOT") \
+            .config("spark.jars.packages", "io.delta:delta-sharing-spark_2.12:0.6.0-SNAPSHOT") \
             .config("spark.delta.sharing.network.sslTrustAll", "true") \
             .getOrCreate()
 
