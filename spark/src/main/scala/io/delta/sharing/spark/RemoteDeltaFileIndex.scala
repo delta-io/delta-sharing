aeb087e463ae455f6cbd9d2447b94cd88e99c6e8
Lin Zhou <lin.zhou@databricks.com>
2022-12-19 14:36:21 -0800

    add comment

--- spark/src/main/scala/io/delta/sharing/spark/RemoteDeltaFileIndex.scala
+++ spark/src/main/scala/io/delta/sharing/spark/RemoteDeltaFileIndex.scala
@@ -77,6 +77,8 @@ private[sharing] abstract class RemoteDeltaFileIndexBase(
   // A helper function to create partition directories from the specified actions.
   protected def makePartitionDirectories(actions: Seq[FileAction]): Seq[PartitionDirectory] = {
     val timeZone = params.spark.sessionState.conf.sessionLocalTimeZone
+    // The getPartitionValuesInDF function is idempotent, and calling it multiple times does not
+    // change its output.
     actions.groupBy(_.getPartitionValuesInDF()).map {
       case (partitionValues, files) =>
         val rowValues: Array[Any] = partitionSchema.map { p =>
