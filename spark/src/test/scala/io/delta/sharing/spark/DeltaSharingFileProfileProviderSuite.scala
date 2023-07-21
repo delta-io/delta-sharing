f58d05fc919c48dfe5753f39447c0eafcb5d6f97
Lin Zhou <lin.zhou@databricks.com>
2023-07-21 14:29:39 -0700

    fix test

--- spark/src/test/scala/io/delta/sharing/spark/DeltaSharingFileProfileProviderSuite.scala
+++ spark/src/test/scala/io/delta/sharing/spark/DeltaSharingFileProfileProviderSuite.scala
@@ -70,8 +70,8 @@ class DeltaSharingFileProfileProviderSuite extends SparkFunSuite {
     )
   }
 
-  test("version is missing") {
-    val e = intercept[Exception] {
+  test("shareCredentialsVersion is missing") {
+    val e = intercept[IllegalArgumentException] {
       testProfile(
         """{
           |  "endpoint": "foo",
