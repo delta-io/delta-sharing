b7c692c9bb4a5ef8932335c6d2b1ecd290fa540b
Lin Zhou <lin.zhou@databricks.com>
2023-09-20 09:51:35 -0700

    add addtitional info in error message

--- spark/src/main/scala/io/delta/sharing/spark/RandomAccessHttpInputStream.scala
+++ spark/src/main/scala/io/delta/sharing/spark/RandomAccessHttpInputStream.scala
@@ -167,7 +167,7 @@ private[sharing] class RandomAccessHttpInputStream(
             }
           }
           throw new UnexpectedHttpStatus(
-            s"HTTP request failed with status: $status $errorBody",
+            s"HTTP request failed with status: $status $errorBody. While accessing [$uri]",
             statusCode)
         }
         entity
