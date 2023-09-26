569adb31a4ea6a2d597d506e0231bf9013237874
Antonio Murgia <antonio.murgia@agilelab.it>
2023-09-26 17:38:48 +0200

    ninja: Remove useless replicated openapi plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,6 @@ pluginManagement {
     }
     plugins {
         id(quarkusPluginId) version quarkusPluginVersion
-        id("org.openapi.generator") version "6.6.0"
         id("com.palantir.git-version") version "3.0.0"
         id("com.diffplug.spotless") version "6.21.0"
     }
