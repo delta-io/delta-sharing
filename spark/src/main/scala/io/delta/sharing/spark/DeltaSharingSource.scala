6a3afe33c26fd5d22430df09ce322012244303b4
Lin Zhou <lin.zhou@databricks.com>
2023-11-30 16:03:18 -0800

    tmp

--- spark/src/main/scala/io/delta/sharing/spark/DeltaSharingSource.scala
+++ spark/src/main/scala/io/delta/sharing/spark/DeltaSharingSource.scala
@@ -321,6 +321,10 @@ case class DeltaSharingSource(
       }
 
       val numFiles = tableFiles.files.size
+      logInfo(
+        s"Fetched ${numFiles} files for table version ${tableFiles.version} from" +
+          " delta sharing server."
+      )
       tableFiles.files.sortWith(fileActionCompareFunc).zipWithIndex.foreach {
         case (file, index) if (index > fromIndex) =>
           appendToSortedFetchedFiles(
@@ -362,6 +366,11 @@ case class DeltaSharingSource(
         TableRefreshResult(idToUrl, None)
       }
       val allAddFiles = validateCommitAndFilterAddFiles(tableFiles).groupBy(a => a.version)
+      logInfo(
+        s"Fetched and filtered ${allAddFiles.size} files from startingVersion " +
+          s"${fromVersion} to endingVersion ${endingVersionForQuery} from " +
+          "delta sharing server."
+      )
       for (v <- fromVersion to endingVersionForQuery) {
 
         val vAddFiles = allAddFiles.getOrElse(v, ArrayBuffer[AddFileForCDF]())
