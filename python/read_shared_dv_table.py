44ebd906187cf894d0cf6f7c11a9b3877c8c361d
Lin Zhou <lin.zhou@databricks.com>
2024-03-15 13:38:10 -0700

    test passed

--- python/read_shared_dv_table.py
+++ python/read_shared_dv_table.py
@@ -19,46 +19,43 @@ import sys
 
 import os
 import json
-from datetime import date, datetime
-from typing import Optional, Sequence
+from datetime import datetime
 
-import pandas as pd
-
-from delta_sharing.protocol import AddFile, AddCdcFile, CdfOptions, DeltaSharingProfile, Metadata, RemoveFile, Share, Table
+from delta_sharing.protocol import DeltaSharingProfile, Share, Table
 from delta_sharing.reader import DeltaSharingReader
 from delta_sharing.rest_client import (
-    ListFilesInTableResponse,
-    ListTableChangesResponse,
-    DataSharingRestClient,
+    DataSharingRestClient
 )
-from delta_sharing.tests.conftest import ENABLE_INTEGRATION, SKIP_MESSAGE
 
 
 def test_read_delta_sharing_dv_table():
-    profile = DeltaSharingProfile.from_json('{"shareCredentialsVersion":1,"bearerToken":"N8mpD9kLhjvU62d5g-d-qsofhiSvq7NLBfhmTnP2C6eWaI9V8DfMA6Sak4mbU5Y6","endpoint":"https://oregon.staging.cloud.databricks.com/api/2.0/delta-sharing/metastores/19a85dee-54bc-43a2-87ab-023d0ec16013","expirationTime":"9999-12-31T23:59:59.999Z"}')
+    profile = DeltaSharingProfile.from_json('{"shareCredentialsVersion":1,"bearerToken":"xx","endpoint":"https://oregon.staging.cloud.databricks.com/api/2.0/delta-sharing/metastores/19a85dee-54bc-43a2-87ab-023d0ec16013","expirationTime":"9999-12-31T23:59:59.999Z"}')
     rest_client = DataSharingRestClient(profile)
     print("----[linzhou]----START-")
     delta_log_dir = "delta_log_for_dv_table_" + datetime.now().strftime("%Y%m%d_%H%M%S")
     os.mkdir(delta_log_dir)
     os.chdir(delta_log_dir)
-    print("----[linzhou]----getcwd-", os.getcwd())
+    table_path = "file:///" + os.getcwd()
+    os.mkdir("_delta_log")
+    os.chdir("_delta_log")
+    print("----[linzhou]----_delta_log:", os.getcwd())
 
 #     print(rest_client.list_shares())
-#     print(rest_client.list_all_tables(Share(name = "lin_dvsharing_bugbash_share_20231113")))
+    allTables = rest_client.list_all_tables(Share(name = "lin_dvsharing_bugbash_share_20231113")).tables
+    for eachTable in allTables:
+        print(eachTable)
     rest_client.set_delta_format_header()
-    filesResponse = rest_client.list_files_in_table(Table("dv_table", "lin_dvsharing_bugbash_share_20231113", "regular_schema"))
+    filesResponse = rest_client.list_files_in_table(Table("hack_dv_table", "lin_dvsharing_bugbash_share_20231113", "regular_schema"))
     f = open("0".zfill(20) + ".json", "w+")
 
     lines = filesResponse.lines
     protocol_json = json.loads(lines.pop(0))
-    print("----[linzhou]----protocol_json:", protocol_json)
     deltaProtocol = {"protocol": protocol_json["protocol"]["deltaProtocol"]}
     json.dump(deltaProtocol, f)
     f.write("\n")
 
     metadata_json = json.loads(lines.pop(0))
-    print("----[linzhou]----metadata_json:", metadata_json)
-    deltaMetadata = {"metadata": metadata_json["metaData"]["deltaMetadata"]}
+    deltaMetadata = {"metaData": metadata_json["metaData"]["deltaMetadata"]}
     json.dump(deltaMetadata, f)
     f.write("\n")
     for line in lines:
@@ -66,6 +63,19 @@ def test_read_delta_sharing_dv_table():
         json.dump(file_json["file"]["deltaSingleAction"], f)
         f.write("\n")
     f.close()
+    print("----[linzhou]----MID---")
+
+    print("----[linzhou]----tablepath:", table_path)
+
+    interface = delta_kernel_python.PythonInterface(table_path)
+    table = delta_kernel_python.Table(table_path)
+    snapshot = table.snapshot(interface)
+    print("Table Version %i" % snapshot.version())
+
+    scan = delta_kernel_python.ScanBuilder(snapshot).build()
+    table = pa.Table.from_batches(scan.execute(interface))
+    print(table.to_pandas())
+
     print("----[linzhou]----END---")
 
 
