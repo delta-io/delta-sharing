fbe1cea0f3ac703e0bcfcdd18f6432026ecda8c2
Sharad Singh <sharadsingh8@gmail.com>
2024-03-25 07:40:56 +0530

    push
    push one

--- server/src/test/scala/io/delta/sharing/server/TestResource.scala
+++ server/src/test/scala/io/delta/sharing/server/TestResource.scala
@@ -58,6 +58,7 @@ object TestResource {
   }
 
   def setupTestTables(): File = {
+    
     val testConfigFile = Files.createTempFile("delta-sharing", ".yaml").toFile
     testConfigFile.deleteOnExit()
     maybeSetupGoogleServiceAccountCredentials
