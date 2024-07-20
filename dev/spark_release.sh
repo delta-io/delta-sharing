c0016496c663c321d12f8497bf22570d3cdb2ab0
Lin Zhou <lin.zhou@databricks.com>
2024-07-20 10:19:04 +0800

    clean up

--- dev/spark_release.sh
+++ dev/spark_release.sh
@@ -11,10 +11,6 @@ git clean -fdx
 
 # Clean existing artifacts
 build/sbt clean
-#cd python
-#python3 setup.py clean --all
-#rm -rf delta_sharing.egg-info dist
-#cd ..
 
 printf "Please type the release version: "
 read VERSION
@@ -24,21 +20,5 @@ build/sbt "release skip-tests"
 
 # Switch to the release commit
 git checkout v$VERSION
-
-# Generate the pre-built server package and sign files
-#build/sbt server/universal:packageBin
-#cd server/target/universal
-#gpg --detach-sign --armor --sign delta-sharing-server-$VERSION.zip
-#gpg --verify delta-sharing-server-$VERSION.zip.asc
-#sha256sum delta-sharing-server-$VERSION.zip > delta-sharing-server-$VERSION.zip.sha256
-#sha256sum -c delta-sharing-server-$VERSION.zip.sha256
-#sha256sum delta-sharing-server-$VERSION.zip.asc > delta-sharing-server-$VERSION.zip.asc.sha256
-#sha256sum -c delta-sharing-server-$VERSION.zip.asc.sha256
-#cd -
-
-# Build the docker image
-#build/sbt server/docker:publish
-
-#git checkout main
 
 echo "=== Generated all release artifacts ==="
