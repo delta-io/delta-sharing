8aa273e55d640e2a4871b634ca49a2daacca306e
Lin Zhou <lin.zhou@databricks.com>
2024-08-13 09:29:50 +0800

    add spark_release.sh

---
+++ dev/spark_release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e -pipe
+
+export GPG_TTY=$(tty)
+
+# Switch to the project root directory
+cd $( dirname $0 )
+cd ..
+
+# Clean up uncommitted files
+git clean -fdx
+
+# Clean existing artifacts
+build/sbt clean
+
+printf "Please type the release version: "
+read VERSION
+echo $VERSION
+
+build/sbt "release skip-tests"
+
+# Switch to the release commit
+git tag v$VERSION
+
+echo "=== Generated all release artifacts ==="
