c6ce5cb69444e85a3b57a41b53365acd956940d0
Moe Derakhshani <moe.derakhshani@databricks.com>
2024-12-29 15:02:25 -0800

    fixed test

--- python/delta_sharing/tests/test_oauth_client.py
+++ python/delta_sharing/tests/test_oauth_client.py
@@ -42,17 +42,22 @@ def mock_server():
     yield server
 
 
-@pytest.mark.parametrize("response_data, expected_expires_in, access_token", [
+@pytest.mark.parametrize("response_data, expected_expires_in, expected_access_token", [
     # OAuth spec requires 'expires_in' to be an integer, e.g., 3600.
     # See https://datatracker.ietf.org/doc/html/rfc6749#section-5.1
     # But some token endpoints return `expires_in` as a string e.g., "3600".
     # This test ensures the client can handle such cases.
     # The test case ensures that we support both integer and string values for 'expires_in' field.
-    ('{"access_token": "test-access-token", "expires_in": 3600, "token_type": "bearer"}',
-     3600,
-     "test-access-token"),
-    ('{"access_token": "test-access-token", "expires_in": "3600", "token_type": "bearer"}',
-     3600, "test-access-token")
+    (
+            '{"access_token": "test-access-token", "expires_in": 3600, "token_type": "bearer"}',
+            3600,
+            "test-access-token"
+    ),
+    (
+            '{"access_token": "test-access-token", "expires_in": "3600", "token_type": "bearer"}',
+            3600,
+            "test-access-token"
+     )
 ])
 def test_oauth_client_should_parse_token_response_correctly(mock_server,
                                                             response_data,
