95d446fa7323edc4a8c288c7e0803e0297f2d064
littlegrasscao <45191106+littlegrasscao@users.noreply.github.com>
2025-03-09 18:27:18 -0700

    Fix RemoteDeltaLogSuite failure (#659)
    * fix TestDeltaSharingClient
    
    * reduce retry times in test
    
    ---------
    
    Co-authored-by: Sun Cao <sun.cao+data@databricks.com>

--- spark/src/test/scala/io/delta/sharing/spark/TestDeltaSharingClient.scala
+++ spark/src/test/scala/io/delta/sharing/spark/TestDeltaSharingClient.scala
@@ -39,8 +39,9 @@ import io.delta.sharing.spark.TestDeltaSharingClient.TESTING_TIMESTAMP
 class TestDeltaSharingClient(
     profileProvider: DeltaSharingProfileProvider = new TestDeltaSharingProfileProvider,
     timeoutInSeconds: Int = 120,
-    numRetries: Int = 10,
+    numRetries: Int = 3,
     maxRetryDuration: Long = Long.MaxValue,
+    retrySleepInterval: Long = 1000,
     sslTrustAll: Boolean = false,
     forStreaming: Boolean = false,
     responseFormat: String = DeltaSharingOptions.RESPONSE_FORMAT_PARQUET,
