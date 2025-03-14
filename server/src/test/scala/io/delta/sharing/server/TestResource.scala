11153173882bf2577d2ab070b1bdadbcbcfeda26
Patrick Jin <patrick.jin@databricks.com>
2025-03-14 09:58:45 -0700

    Update python branch 1.3 (#661)
    * upgrade delta-kernel-rs to 0.6.1 to fix reading tables partitioned on timestamp (#634)
    
    * include source in maturin build of delta-kernel-rust-sharing-wrapper (#647)
    
    * upgrade actions/cache to v4 (#655)
    
    * Support delta format for table protocol and metadata (#657)
    
    * timestampntz support (#654)
    
    * Fix timestampntz test (#662)
    
    * Update listAllTables test with timestampntz_cdf_table (#663)

--- server/src/test/scala/io/delta/sharing/server/TestResource.scala
+++ server/src/test/scala/io/delta/sharing/server/TestResource.scala
@@ -207,6 +207,12 @@ object TestResource {
             "default",
             java.util.Arrays.asList(
               TableConfig(
+                "timestampntz_cdf_table",
+                s"s3a://${AWS.bucket}/delta-exchange-test/timestampntz_cdf_table",
+                "00000000-0000-0000-0000-000000000096",
+                historyShared = true
+              ),
+              TableConfig(
                 "cdf_table_cdf_enabled",
                 s"s3a://${AWS.bucket}/delta-exchange-test/cdf_table_cdf_enabled",
                 "00000000-0000-0000-0000-000000000095",
