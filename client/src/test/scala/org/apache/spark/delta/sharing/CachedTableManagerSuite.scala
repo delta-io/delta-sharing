abe4458c718d356fb35b643fc4a081f3ce5d7d52
Sun Cao <sun.cao+data@databricks.com>
2025-05-09 11:02:19 -0700

    update format

--- client/src/test/scala/org/apache/spark/delta/sharing/CachedTableManagerSuite.scala
+++ client/src/test/scala/org/apache/spark/delta/sharing/CachedTableManagerSuite.scala
@@ -325,17 +325,17 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
     val fileId = "file1"
     val url = "https://test.com/file1"
     val queryId = "query1"
-    
+
     val refresherWrapper: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => refresher(token)
-    
+
     val refresher: Option[String] => TableRefreshResult = _ =>
       TableRefreshResult(
         Map(fileId -> url), Some(System.currentTimeMillis() + preSignedUrlExpirationMs), None)
-    
+
     val profileProvider = createProfileProvider(queryId, refresherWrapper)
     val ref = new WeakReference[AnyRef](new Object())
-    
+
     val expectedExpiration = System.currentTimeMillis() + preSignedUrlExpirationMs
     manager.register(
       tablePath,
@@ -346,7 +346,7 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
       expectedExpiration,
       None
     )
-    
+
     val (retrievedUrl, actualExpiration) = manager.getPreSignedUrl(tablePath, fileId)
     assert(retrievedUrl === url)
     // The actual expiration should be at least as large as the expected expiration
@@ -362,22 +362,22 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
     val fileId = "file1"
     val initialUrl = "https://test.com/file1"
     val refreshedUrl = "https://test.com/file1-refreshed"
-    
+
     // Track which wrapper was used for refresh
     var lastUsedWrapper: String = "none"
-    
+
     val refresherWrapper1: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => {
         lastUsedWrapper = "wrapper1"
         refresher(token)
       }
-    
+
     val refresherWrapper2: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => {
         lastUsedWrapper = "wrapper2"
         refresher(token)
       }
-    
+
     // Single shared refresh function
     val refresher: Option[String] => TableRefreshResult = _ => {
       val newExpiration = System.currentTimeMillis() + refreshThresholdMs + 100
@@ -386,12 +386,12 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
         Some(newExpiration),
         None)
     }
-    
+
     // Register first query with short expiration
     val queryId1 = "query1"
     val profileProvider1 = createProfileProvider(queryId1, refresherWrapper1)
     val ref1 = new WeakReference[AnyRef](new Object())
-    
+
     manager.register(
       tablePath,
       Map(fileId -> initialUrl),
@@ -401,12 +401,12 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
       System.currentTimeMillis() + refreshThresholdMs + 100, // Short expiration
       None
     )
-    
+
     // Register second query with short expiration
     val queryId2 = "query2"
     val profileProvider2 = createProfileProvider(queryId2, refresherWrapper2)
     val ref2 = new WeakReference[AnyRef](new Object())
-    
+
     manager.register(
       tablePath,
       Map(fileId -> initialUrl),
@@ -418,33 +418,33 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
     )
 
     assert(manager.size == 1)
-    
+
     // Sleep to let the URLs expire
     Thread.sleep(200)
-    
+
     // Trigger refresh - only one wrapper should be used
     manager.refresh()
-    
+
     // Verify only one wrapper was used
     assert(lastUsedWrapper === "wrapper1" || lastUsedWrapper === "wrapper2")
-    
+
     // Get URL - should be from the shared refresher
     val (retrievedUrl, _) = manager.getPreSignedUrl(tablePath, fileId)
     assert(retrievedUrl === refreshedUrl)
-    
+
     // Clear first query's reference
     ref1.clear()
-    
+
     // Sleep again to let the URLs expire
     Thread.sleep(200)
-    
+
     // Trigger refresh again - now only second wrapper should be used
     manager.refresh()
-    
+
     // Verify second wrapper was used
     assert(manager.size == 1)
     assert(lastUsedWrapper === "wrapper2")
-    
+
     // URL should still be accessible
     val (retrievedUrl2, _) = manager.getPreSignedUrl(tablePath, fileId)
     assert(retrievedUrl2 === refreshedUrl)
@@ -470,23 +470,23 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
     val initialUrl2 = "https://test.com/file2"
     val refreshedUrl1 = "https://test.com/file1-refreshed"
     val refreshedUrl2 = "https://test.com/file2-refreshed"
-    
+
     // Track refresh states for each table
     var table1RefreshCount = 0
     var table2RefreshCount = 0
-    
+
     val refresherWrapper1: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => {
         table1RefreshCount += 1
         refresher(token)
       }
-    
+
     val refresherWrapper2: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => {
         table2RefreshCount += 1
         refresher(token)
       }
-    
+
     // Different refresh functions for different tables
     val refresher1: Option[String] => TableRefreshResult = _ => {
       val newExpiration = System.currentTimeMillis() + refreshThresholdMs + 100
@@ -495,7 +495,7 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
         Some(newExpiration),
         None)
     }
-    
+
     val refresher2: Option[String] => TableRefreshResult = _ => {
       val newExpiration = System.currentTimeMillis() + refreshThresholdMs + 100
       TableRefreshResult(
@@ -503,12 +503,12 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
         Some(newExpiration),
         None)
     }
-    
+
     // Register first query for first table with short expiration
     val queryId1 = "query1"
     val profileProvider1 = createProfileProvider(queryId1, refresherWrapper1)
     val ref1 = new WeakReference[AnyRef](new Object())
-    
+
     manager.register(
       tablePath1,
       Map(fileId -> initialUrl1),
@@ -518,12 +518,12 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
       System.currentTimeMillis() + refreshThresholdMs + 100, // Short expiration
       None
     )
-    
+
     // Register second query for second table with short expiration
     val queryId2 = "query2"
     val profileProvider2 = createProfileProvider(queryId2, refresherWrapper2)
     val ref2 = new WeakReference[AnyRef](new Object())
-    
+
     manager.register(
       tablePath2,
       Map(fileId -> initialUrl2),
@@ -533,46 +533,46 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
       System.currentTimeMillis() + refreshThresholdMs + 100, // Short expiration
       None
     )
-    
+
     assert(manager.size == 2)
-    
+
     // Sleep to let the URLs expire
     Thread.sleep(200)
-    
+
     // Trigger refresh - both tables should be refreshed since URLs have expired
     manager.refresh()
-    
+
     // Verify both tables were refreshed
     assert(table1RefreshCount === 1)
     assert(table2RefreshCount === 1)
-    
+
     // Get URLs - should be from their respective refreshers
     val (retrievedUrl1, _) = manager.getPreSignedUrl(tablePath1, fileId)
     val (retrievedUrl2, _) = manager.getPreSignedUrl(tablePath2, fileId)
     assert(retrievedUrl1 === refreshedUrl1)
     assert(retrievedUrl2 === refreshedUrl2)
-    
+
     // Clear first query's reference
     ref1.clear()
-    
+
     // Sleep again to let the URLs expire
     Thread.sleep(200)
-    
+
     // Trigger refresh again - only second table should be refreshed
     manager.refresh()
-    
+
     // Verify only second table was refreshed again
     assert(table1RefreshCount === 1) // Should not change
     assert(table2RefreshCount === 2) // Should increment
-    
+
     // First table should be removed from cache
     assert(manager.size == 1)
-    
+
     // First table's URL should no longer be accessible
     intercept[IllegalStateException] {
       manager.getPreSignedUrl(tablePath1, fileId)
     }
-    
+
     // Second table's URL should still be accessible
     val (retrievedUrl2Again, _) = manager.getPreSignedUrl(tablePath2, fileId)
     assert(retrievedUrl2Again === refreshedUrl2)
@@ -587,20 +587,20 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
     val fileId = "file1"
     val initialUrl = "https://test.com/file1"
     val refreshedUrl = "https://test.com/file1-refreshed"
-    
+
     val refresherWrapper: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => refresher(token)
-    
+
     val refresher: Option[String] => TableRefreshResult = _ =>
       TableRefreshResult(
         Map(fileId -> refreshedUrl),
         Some(System.currentTimeMillis() + preSignedUrlExpirationMs),
         None
       )
-    
+
     val profileProvider = createProfileProvider("query1", refresherWrapper)
     val ref = new WeakReference[AnyRef](new Object())
-    
+
     // Register with expired timestamp
     manager.register(
       tablePath,
@@ -611,7 +611,7 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
       System.currentTimeMillis(), // Already expired
       None
     )
-    
+
     // Verify URL was refreshed immediately
     val (retrievedUrl, _) = manager.getPreSignedUrl(tablePath, fileId)
     assert(retrievedUrl === refreshedUrl)
@@ -627,10 +627,10 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
     val initialUrl = "https://test.com/file1"
     val refreshedUrl = "https://test.com/file1-refreshed"
     val refreshToken = "token123"
-    
+
     val refresherWrapper: QuerySpecificCachedTable.RefresherWrapper =
       (token, refresher) => refresher(token)
-    
+
     var receivedTokens = Seq.empty[Option[String]]
     val refresher: Option[String] => TableRefreshResult = token => {
       receivedTokens = receivedTokens :+ token
@@ -640,10 +640,10 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
         Some(refreshToken)
       )
     }
-    
+
     val profileProvider = createProfileProvider("query1", refresherWrapper)
     val ref = new WeakReference[AnyRef](new Object())
-    
+
     // Register with no refresh token and short expiration to trigger refresh
     manager.register(
       tablePath,
@@ -654,21 +654,21 @@ class CachedTableManagerSuite extends SparkFunSuite with SharedSparkSession{
       System.currentTimeMillis() + refreshThresholdMs + 100,
       None
     )
-    
+
     // Sleep to let the URLs expire
     Thread.sleep(200)
-    
+
     // First refresh - should receive None as token
     manager.refresh()
     assert(receivedTokens === Seq(None))
-    
+
     // Sleep again to let the URLs expire
     Thread.sleep(200)
-    
+
     // Second refresh - should receive the token from first refresh
     manager.refresh()
     assert(receivedTokens === Seq(None, Some(refreshToken)))
-    
+
     // Verify we got the refreshed URL
     val (retrievedUrl, _) = manager.getPreSignedUrl(tablePath, fileId)
     assert(retrievedUrl === refreshedUrl)
