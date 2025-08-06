1003f743b429c38e9b9f1d5350a4f7849343680b
Sun Cao <sun.cao+data@databricks.com>
2025-08-06 16:10:52 -0700

    Update spark_release.sh

--- dev/spark_release.sh
+++ dev/spark_release.sh
@@ -27,13 +27,16 @@ if build/sbt "release skip-tests"; then
   git tag v$VERSION
   echo "Created git tag: v$VERSION"
 
-  # Transfer deployment to Central Publisher Portal (required for Maven-API-like plugins)
+  # Transfer deployment to Central Publisher Portal
   echo "Transferring deployment to Central Publisher Portal..."
   NAMESPACE="io.delta"
   OSSRH_BASE="https://ossrh-staging-api.central.sonatype.com"
 
   # Make POST request to upload to Portal
   # This must be done from the same IP that was used for publishing
+  # You need these environment variables to be set:
+  # export SONATYPE_USERNAME=your-username
+  # export SONATYPE_PASSWORD=your-password
   curl -X POST \
     -H "Authorization: Bearer $(echo -n "$SONATYPE_USERNAME:$SONATYPE_PASSWORD" | base64)" \
     "$OSSRH_BASE/manual/upload/defaultRepository/$NAMESPACE" \
