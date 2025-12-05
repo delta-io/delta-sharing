01455723565675d0841f503f8b47932b4e67cd32
littlegrasscao <45191106+littlegrasscao@users.noreply.github.com>
2025-12-04 16:27:45 -0800

    Add configuration to guard DeltaSharingOptionsProfileProvider usage (#824) (#827)
    * impl
    
    
    
    * Update DeltaSharingClient.scala
    
    
    
    * Update DeltaSharingRestClientSuite.scala
    
    
    
    * Update DeltaSharingRestClientSuite.scala
    
    
    
    ---------
    
    Co-authored-by: Sun Cao <sun.cao+data@databricks.com>

--- client/src/main/scala/io/delta/sharing/client/util/ConfUtils.scala
+++ client/src/main/scala/io/delta/sharing/client/util/ConfUtils.scala
@@ -87,6 +87,9 @@ object ConfUtils {
   val STRUCTURAL_SCHEMA_MATCH_CONF = "spark.delta.sharing.client.useStructuralSchemaMatch"
   val STRUCTURAL_SCHEMA_MATCH_DEFAULT = "false"
 
+  val OPTIONS_PROFILE_PROVIDER_ENABLED_CONF = "spark.delta.sharing.profile.optionsProvider.enabled"
+  val OPTIONS_PROFILE_PROVIDER_ENABLED_DEFAULT = true
+
   def getProxyConfig(conf: Configuration): Option[ProxyConfig] = {
     val proxyHost = conf.get(PROXY_HOST, null)
     val proxyPortAsString = conf.get(PROXY_PORT, null)
@@ -239,6 +242,17 @@ object ConfUtils {
 
   def structuralSchemaMatchingEnabled(conf: SQLConf): Boolean =
     conf.getConfString(STRUCTURAL_SCHEMA_MATCH_CONF, STRUCTURAL_SCHEMA_MATCH_DEFAULT).toBoolean
+
+  def optionsProfileProviderEnabled(conf: Configuration): Boolean = {
+    conf.getBoolean(
+      OPTIONS_PROFILE_PROVIDER_ENABLED_CONF, OPTIONS_PROFILE_PROVIDER_ENABLED_DEFAULT)
+  }
+
+  def optionsProfileProviderEnabled(conf: SQLConf): Boolean = {
+    conf.getConfString(
+      OPTIONS_PROFILE_PROVIDER_ENABLED_CONF,
+      OPTIONS_PROFILE_PROVIDER_ENABLED_DEFAULT.toString).toBoolean
+  }
 
   private def toTimeInSeconds(timeStr: String, conf: String): Int = {
     val timeInSeconds = JavaUtils.timeStringAs(timeStr, TimeUnit.SECONDS)
