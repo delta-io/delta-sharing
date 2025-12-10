e1020a7ab89c11c07004d6e80a35e4eebc0485e2
Sun Cao <sun.cao+data@databricks.com>
2025-12-10 12:18:36 -0800

    Revert "Fix test suites (#805)"
    This reverts commit 310b0295a7bfea982ff61e20bdcf6dae62cd725a.

--- client/src/test/scala/io/delta/sharing/client/util/RetryUtilsSuite.scala
+++ client/src/test/scala/io/delta/sharing/client/util/RetryUtilsSuite.scala
@@ -46,7 +46,7 @@ class RetryUtilsSuite extends SparkFunSuite {
     assert(shouldRetry(new java.net.SocketException("Connection reset by peer")))
     assert(shouldRetry(new java.net.SocketException("CONNECTION RESET")))
     assert(!shouldRetry(new java.net.SocketException("Some other socket error")))
-    assert(!shouldRetry(new java.net.SocketException(null: String)))
+    assert(!shouldRetry(new java.net.SocketException(null)))
   }
 
   test("runWithExponentialBackoff") {
