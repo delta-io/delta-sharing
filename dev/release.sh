8cf73dd28dc448e43b6369fa870ddfed4e308640
Lin Zhou <lin.zhou@databricks.com>
2026-04-10 21:20:53 -0700

    resolve PR comments

--- dev/release.sh
+++ dev/release.sh
@@ -12,8 +12,7 @@ git clean -fdx
 # Clean existing artifacts
 build/sbt clean
 cd python
-rm -rf build dist
-rm -rf delta_sharing.egg-info dist
+rm -rf build dist delta_sharing.egg-info
 cd ..
 
 printf "Please type the release version: "
@@ -33,7 +32,7 @@ git checkout v$VERSION
 
 # Generate Python artifacts
 cd python/
-python3 -m build
+uv build
 cd ..
 
 # Generate the pre-built server package and sign files
